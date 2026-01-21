a7e4c3c385c0041c725f383b6e6e7461914f1db7
Tom Hirschberger <github@hirschberger.eu>
2026-01-21 10:04:15 +0100

    implemented a option to configure the clock pin behavoir (falling, rising) of rotaries

--- node_helper.js
+++ node_helper.js
@@ -469,7 +469,19 @@ module.exports = NodeHelper.create({
 		}
 		if (dataGpio != null){
 			try {
-				let clockWatch = openGPIOChip.watch(curGPIOClockInfo, openGPIOEdge.Rising)
+				clockIsRising = true
+				if (typeof self.config[identifier].clockIsRising !== "undefined"){
+					if (!self.config[identifier].clockIsRising){
+						clockIsRising = false
+					}
+				}
+
+				let clockWatch = null
+				if (clockIsRising){
+					clockWatch = openGPIOChip.watch(curGPIOClockInfo, openGPIOEdge.Rising)
+				} else {
+					clockWatch = openGPIOChip.watch(curGPIOClockInfo, openGPIOEdge.Falling)
+				}
 				self.registeredPins[clockPin] = clockWatch
 
 				clockWatch.on('change', () => {
